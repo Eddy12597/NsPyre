1feb8f61e348d278c7949b37bd54918dc3c85ad8
Eddy12597 <eddy12597@163.com>
2025-08-13 16:59:20 +0800

    added print(f()) implementation

--- STATUS.md
+++ STATUS.md
@@ -1,21 +1,23 @@
-## Good first issues / status:
+## Good first issues / status
+
+Here the issues and todos are listed with each file. (#) indicates expected difficulty
 
 ### Utils
 `log.py`:
 
 (1): small bug in `level` function 
 (1): add typing support / warning ignores if working on ide / pylance
-(2): needs convenient way of effectively keeping track of file names for log messages from different/multiple files.
+(1): needs convenient way of effectively keeping track of file names for log messages from different/multiple files.
 
 `format.py`:
 
-(1): provide implementation for printf
+~~(1): provide implementation for printf~~
 
 ### Builtins:
 
 `ti_draw.py`: 
 
-(0): needs testing
+(0): needs more thorough testing
 
 `ti_system.py`:
 
--- format.py
+++ format.py
@@ -9,3 +9,31 @@ def substr(s: str, a: int = -1, b: int = 10000000) -> str:
     for i in range(st, end):
         res += s[i]
     return res
+
+
+# can be used with print(f(...)), providing a modern pythonic printf option.
+def f(fmt, *args):
+    i = 0
+    res=""
+    args = list(args)
+    while i < len(fmt):
+        if fmt[i] == '\\':
+            if i + 1 < len(fmt):
+                esc = fmt[i+1]
+                if esc == 'n':
+                    res += "\n"
+                elif esc == 't':
+                    res += '\t'
+                elif esc == '\\':
+                    res += '\\'
+                i += 2
+            else:
+                i += 1
+        elif fmt[i] == '%' and i + 1 < len(fmt) and fmt[i+1] == 'v':
+            if args:
+                res += str(args.pop(0))
+                i += 2
+        else:
+            res += fmt[i]
+            i += 1
+    return res
